bd25d32b1ad59feee30d752c9903fe6f5a34f73e
AnaBeatriz <104109534+httpsbia@users.noreply.github.com>
2022-06-06 22:41:33 -0300

    Create Exercicio01.java

---
+++ Exercicio01.java
@@ -0,0 +1,11 @@
+package vetores1Ao23;
+import java.util.*;
+
+public class Exercicio01 {
+	public static void main(String []args) {
+		int x [] = {30,30,30,30,30,30,30,30,30,30};
+		
+	    System.out.println(x[6]);
+	}
+
+}
