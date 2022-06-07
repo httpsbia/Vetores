10f545c8560bc41aa23b822061efd6f4fbb3abdc
AnaBeatriz <104109534+httpsbia@users.noreply.github.com>
2022-06-06 22:43:58 -0300

    Create Exercicio06.java

---
+++ Exercicio06.java
@@ -0,0 +1,16 @@
+package vetores1Ao23;
+import java.util.*;
+
+public class Exercicio06 {
+	public static void main (String[]args) {
+		int [] D = {1,2,3,4,5,6,7,8,9,10, 54, 329};
+		for (int A : D) {
+		   for (A = D.length - 1; A >= 0; A--) {
+			   System.out.println(D[A]);
+				
+			}
+		}
+		
+	}
+
+}
