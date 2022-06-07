bfa74b0ac4763ed4544e972ca7b362da73df9374
AnaBeatriz <104109534+httpsbia@users.noreply.github.com>
2022-06-06 22:43:21 -0300

    Create Exercicio05.java

---
+++ Exercicio05.java
@@ -0,0 +1,20 @@
+package vetores1Ao23;
+import java.util.*;
+
+public class Exercicio05 {
+	public static void main (String []args) {
+		int [] C = {-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
+		C [0] = 0;
+		C [1] = 0;
+		C [2] = 0;
+		C [3] = 0;
+		C [4] = 0;
+		C [5] = 0;
+		C [6] = 0;
+		C [7] = 0;
+		C [8] = 0;
+		C [9] = 0;
+		
+		System.out.println("vetor " + C [4] + " modificado!");
+	}
+	}
